2618af1fc70504e73011dbf896f471c5f4a968cf
Acksel <egortrifo@gmail.com>
2023-09-16 16:35:03 +0300

    work with templates

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,5 +1,16 @@
+from timeit import default_timer
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def shop_index(request):
-    return HttpResponse("Hello World!")
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999), 
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
