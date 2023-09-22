daf81a3354ab92b10560e1b3ee529f520cf3865f
Acksel <egortrifo@gmail.com>
2023-09-22 17:33:55 +0300

    work with admin

--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -7,9 +7,6 @@ class Product(models.Model):
         # db_table = "tech_products"
         # verbose_name_plural = "products"
 
-    def __str__(self):
-        return self.name
-    
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
@@ -17,9 +14,21 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     archived = models.BooleanField(default=False)
 
+    #@property
+    #def description_short(self) -> str:
+    #    if len(self.description) < 48:
+    #        return self.description
+    #    return self.description[:48] + "..." 
+
+    def __str__(self) -> str:
+        return f"Product(pk={self.pk}, name={self.name!r})"
+
 class Order(models.Model):
     delivery_address = models.TextField(null=True, blank=True)
     promocode = models.CharField(max_length=20, null=False, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     products = models.ManyToManyField(Product, related_name="orders")
+
+    def __str__(self) -> str:
+        return f"Order(pk={self.pk}, address={self.delivery_address!r})"
